99a8e5e5d0a13ac2595d080498ab93d2bb2fd9cc
Matthew Collins <mcollins@acis.ufl.edu>
2017-02-27 17:12:29 -0500

    MAC addresses for ESX have a smaller range so have to use only the last two octets

--- make_csv.py
+++ make_csv.py
@@ -12,10 +12,10 @@ hostname_second_part = range(1, 3+1)
 domainname = "acis.ufl.edu"
 
 # IP address configuration
-start_ip = "10.5.144.1"
+start_ip = "10.244.39.100"
 
 # mac address configuration
-mac_prefix = "00:af:11"
+mac_prefix = "00:50:56:1F"
 
 loop_count = 0
 # header isn't needed for out workflow
@@ -32,8 +32,8 @@ for first in hostname_first_part:
         ip_address = ipaddress.IPv4Address(start_ip) + loop_count
         
         octets = ip_address.exploded.split(".")
-        mac_address = "{0}:{1:02x}:{2:02x}:{3:02x}".format(mac_prefix,
-            int(octets[1]), int(octets[2]), int(octets[3]))
+        mac_address = "{0}:{1:02x}:{2:02x}".format(mac_prefix,
+            int(octets[2]), int(octets[3]))
 
         print(",".join([".".join([hostname, domainname]), hostname,
             ip_address.exploded, mac_address]))
